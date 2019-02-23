19cd13f6f055cc5006e553c36447dfa016537339
R4GN4R <jimchr2001@gmail.com>
2019-02-23 05:18:07 -0800

    Update ggt-update_notifier.sh

--- Linux/ggt-update_notifier.sh
+++ Linux/ggt-update_notifier.sh
@@ -10,157 +10,100 @@ if [ $? -eq 0 ]; then
 
 MAINPOINT(){
 	echo -e "${NC} Please Select your option:"
-GameList="TWRII TWA Software_Inc Foundation CodeCombat Factorio RimWorld while_True_Learn Yorg_io 0_A_D check_notifier_update gr-gamestranslations.eu"
-echo -e "${NC}"
-
-select selected_option in $GameList; do
+	echo
+	echo -e "##############################  MENU  ##################################"
+Options="Show_Translations_Versions check_notifier_update gr-gamestranslations.eu"
+select selected_option in $Options; do
+echo -e "########################################################################"
 echo -e "${ORANGE} You selected $selected_option"
 echo
+if [[ "$selected_option" = "Show_Translations_Versions" ]]; then
+curl -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersionsWin.md
+	echo -e "11) BACK "
+	echo -e "${NC}"
+	echo -e "Do you want to download a translation? (Y/N)"
+	read
 
-if [[ "$selected_option" = "TWRII" ]]; then
-echo -e "${RED}"
-GameCode="TWRII"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS="=" read MyVAR MyVer; do if [ $MyVAR = "$GameCode" ] ; then echo $MyVer; fi ; done
-echo
 
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/twrii/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
+##########################################################################################################---
 
-elif [[ "$selected_option" = "TWA" ]]; then
-echo -e "${RED}"
-GameCode="TWA"
-curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/twa/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
+	if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
+		SELECTGAME(){
+		printf "Please choose what translation you want:"; read
+		if [[ "$REPLY" = "1" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/twrii/ &>/dev/null
+		elif [[ "$REPLY" = "2" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/s-i/ &>/dev/null
+		elif [[ "$REPLY" = "3" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/twa/ &>/dev/null
+		elif [[ "$REPLY" = "4" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/fdt/ &>/dev/null
+		elif [[ "$REPLY" = "5" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/cc/ &>/dev/null
+		elif [[ "$REPLY" = "6" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/fctr/ &>/dev/null
+		elif [[ "$REPLY" = "7" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/rw/ &>/dev/null
+		elif [[ "$REPLY" = "8" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/wtl/ &>/dev/null
+		elif [[ "$REPLY" = "9" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/yorg/ &>/dev/null
+		elif [[ "$REPLY" = "10" ]]; then
+			xdg-open http://www.gr-gamestranslations.eu/index.php/category/0ad/ &>/dev/null
+		elif [[ "$REPLY" = "11" ]]; then
+			clear
+			MAINPOINT
+		else
+			echo -e "${RED}Invalid option! Please try again.${NC}"
+			SELECTGAME
+		fi
+	}
+	SELECTGAME
+	fi
 
-elif [[ "$selected_option" = "Software_Inc" ]]; then
-echo -e "${RED}"
-GameCode="SI"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/s-i/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
+##########################################################################################################___
 
-elif [[ "$selected_option" = "Foundation" ]]; then
-echo -e "${RED}"
-GameCode="Fndt"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/fdt/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
 
-elif [[ "$selected_option" = "CodeCombat" ]]; then
-echo -e "${RED}"
-GameCode="CodeC"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/cc/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
 
-elif [[ "$selected_option" = "Factorio" ]]; then
-echo -e "${RED}"
-GameCode="Fctr"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/fctr/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
 
-elif [[ "$selected_option" = "RimWorld" ]]; then
-echo -e "${RED}"
-GameCode="RW"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/rw/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
 
-elif [[ "$selected_option" = "while_True_Learn" ]]; then
-echo -e "${RED}"
-GameCode="wTl"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/wtl/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
 
-elif [[ "$selected_option" = "Yorg_io" ]]; then
-echo -e "${RED}"
-GameCode="Yorg"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/yorg/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
 
-elif [[ "$selected_option" = "0_A_D" ]]; then
-echo -e "${RED}"
-GameCode="0AD"; curl -s -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/TranslationVersions.md | while IFS='=' read MyVAR MyVer; do if [ $MyVAR = $GameCode ] ; then echo $MyVer; fi ; done
-echo -e "${NC}Do you want to download it?(Y/N)"
-read
-if [[ "$REPLY" = "Y" || "$REPLY" = "y" ]]; then
-	xdg-open http://www.gr-gamestranslations.eu/index.php/category/0ad/ &>/dev/null
-else
-MAINPOINT
-fi
-echo -e "${NC}"
 
-elif [[ "$selected_option" = "gr-gamestranslations.eu" ]]; then
-xdg-open http://www.gr-gamestranslations.eu/ &>/dev/null
-echo -e "${NC}"
+
+##########################################################################################################---
 
 elif [[ "$selected_option" = "check_notifier_update" ]]; then
 echo -e "Current Version: $CurrentVersion"
 echo -e "${RED}"
 curl -L https://raw.githubusercontent.com/JimChrModder/gr-gamestranslations/master/ggt_online_version.md
 echo -e "${NC}"
+MAINPOINT
+##########################################################################################################___
 
+
+##########################################################################################################---
+
+elif [[ "$selected_option" = "gr-gamestranslations.eu" ]]; then
+xdg-open http://www.gr-gamestranslations.eu/ &>/dev/null
+MAINPOINT
+##########################################################################################################___
+
+##########################################################################################################---
 else
 echo -e "${ORANGE} This option is invalid. Please try again. ${NC}"
-
+MAINPOINT
 fi
+##########################################################################################################___
+
 done
 }
 MAINPOINT	
+
 
 else	
 
 echo -e "${ORANGE}There is no internet connection. Please check your internet settings"
 echo -e	"${NC}"								
 
-fi
+fi 
